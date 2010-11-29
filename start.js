701145e527bff01fd5328c7497142f9838dd2706
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2010-11-29 11:03:02 -0500

    Removing unused code. URL-rewriting was moved to end.js

--- start.js
+++ start.js
@@ -3,22 +3,6 @@ function handleLoadStartEvent(event) {
   safari.self.tab.dispatchMessage("http.load", { location: document.location, cookies: document.cookie });
 }
 
-function fixReferences() {
-  var hostname = document.location.hostname.replace(/\./g, "\\.");
-  
-  var linkSelector = "a[href^=http\\:\\/\\/" + hostname + "]";
-  var insecureLinks = document.querySelectorAll(linkSelector);
-  for (var i = 0; i < insecureLinks.length; i++) {
-    insecureLinks[i].href = insecureLinks[i].href.replace(/^http:/, "https:");
-  }
-
-  var formSelector = "form[action^=http\\:\\/\\/" + hostname + "]";
-  var insecureForms = document.querySelectorAll(formSelector);
-  for (var i = 0; i < insecureForms.length; i++) {
-    insecureForms[i].action = insecureForms[i].action.replace(/^http:/, "https:");
-  }
-}
-
 function handleMessageEvent(event) {
   if (event.name == "https.redirect") {
     document.location = event.message;
